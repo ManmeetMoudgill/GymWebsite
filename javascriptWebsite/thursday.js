b6721f9b35bbf64b38fee90890eb284adb9d39d4
Manmeet Moudgill <75222146+ManmeetMoudgill@users.noreply.github.com>
2021-01-01 15:20:32 +0100

    Update thursday.js

--- javascriptWebsite/thursday.js
+++ javascriptWebsite/thursday.js
@@ -29,7 +29,7 @@ function thursday() {
 
   //creating an image an appending to first inner div
   var img = document.createElement("img");
-  img.src = "../imagesWebsite/fit-cycling.svg";
+  img.src = "/GymWebsite/imagesWebsite/fit-cycling.svg";
   img.className = "logophoto";
   document.getElementById("innerDivJ1").appendChild(img);
 
@@ -57,7 +57,7 @@ function thursday() {
 
   //creating an image an appending to second inner div
   var img2 = document.createElement("img");
-  img2.src = "../imagesWebsite/fit-yoga.svg";
+  img2.src = "/GymWebsite/imagesWebsite/fit-yoga.svg";
   img2.className = "logophoto";
   document.getElementById("innerDivJ2").appendChild(img2);
 
@@ -85,7 +85,7 @@ function thursday() {
 
   //creating an image an appending to third inner div
   var img3 = document.createElement("img");
-  img3.src = "../imagesWebsite/fit-boxing.svg";
+  img3.src = "/GymWebsite/imagesWebsite/fit-boxing.svg";
   img3.className = "logophoto";
   document.getElementById("innerDivJ3").appendChild(img3);
 
@@ -116,7 +116,7 @@ function thursday() {
 
   //creating an image an appending to third inner div
   var img4 = document.createElement("img");
-  img4.src = "../imagesWebsite/doubmell.svg";
+  img4.src = "/GymWebsite/imagesWebsite/doubmell.svg";
   img4.className = "logophoto";
   document.getElementById("innerDivJ4").appendChild(img4);
 
